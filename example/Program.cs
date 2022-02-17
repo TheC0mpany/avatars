c722154cd780b29e10934c5c4d2eac03b9779ad7
TheC0mpany <65458800+TheC0mpany@users.noreply.github.com>
2022-02-17 04:03:35 +0200

    Add files via upload

---
+++ example/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //                         ⬇ Base url where all images stored             ⬇ Generate random number    ⬇ extension
+            string url = "https://github.com/TheC0mpany/avatars/raw/main/avatars/(" + new Random().Next(4729) + ").jpg";
+
+            Console.WriteLine(url);
+
+            Console.ReadLine();
+        }
+    }
+}
